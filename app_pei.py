11c296da3e64c57d803e6fa98d08365f5fccc822
victorgallo14-creator <victorgallo14@gmail.com>
2026-02-13 21:41:06 -0300

    Update app_pei.py

--- app_pei.py
+++ app_pei.py
@@ -200,7 +200,7 @@ def load_db():
 
 def save_student(doc_type, name, data):
     """Salva ou atualiza garantindo que não duplique linhas"""
-    try:
+    try
         df_atual = load_db()
         
         # 1. Geramos o ID único exatamente como ele aparece na lista
@@ -397,12 +397,10 @@ def carregar_dados_aluno():
         st.info("Pronto para novo preenchimento.")
 
 # --- ABERTURA DA PRIMEIRA CAIXA BRANCA ---
-# Adicionei o ) que faltava no final da linha abaixo
-
 col_txt, col_user = st.columns([2, 1])
 
 with col_txt:
-    st.markdown('<h1 style="margin:0;">SISTEMA INTEGRA RAFAEL</h1>', unsafe_allow_html=True)
+    st.markdown('<h1 style="margin:-100;">SISTEMA INTEGRA RAFAEL</h1>', unsafe_allow_html=True)
     st.markdown('<p style="font-size:1.2rem; color:#64748b;">Gestão de Educação Especial</p>', unsafe_allow_html=True)
 
 with col_user:
@@ -1599,6 +1597,7 @@ if st.sidebar.checkbox("👁️ Ver Histórico (Diretor)"):
     df_logs = conn.read(worksheet="Log", ttl=0)
     # Mostra os mais recentes primeiro
     st.dataframe(df_logs.sort_values(by="data_hora", ascending=False), use_container_width=True)
+
 
 
 

3adbd7be58fa7aed341b69f4dbfc1c5efe267257
victorgallo14-creator <victorgallo14@gmail.com>
2026-04-15 16:31:06 -0300

    Update app_pei.py

--- app_pei.py
+++ app_pei.py
@@ -425,11 +425,10 @@ def login():
         # Ajuste de proporção para ficar elegante
         c_pad1, c_art, c_form, c_pad2 = st.columns([1, 4, 4, 1])
         
-        # --- LADO ESQUERDO (ARTE AZUL) ---
 # --- LADO ESQUERDO (ARTE AZUL) ---
         with c_art:
             st.markdown("""
-<div class="login-art-box" style="display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center;">
+<div class="login-art-box" style="display: flex; flex-direction: column; align-items: center; justify-content: center; text-align: center; height: 100%; min-height: 520px;">
 <div style="font-size: 6rem; margin-bottom: 1rem; filter: drop-shadow(0px 4px 6px rgba(0,0,0,0.2));">🧠</div>
 <h1 style="color: white; font-weight: 800; font-size: 3.5rem; margin: 0; line-height: 1;">INTEGRA</h1>
 <p style="font-size: 1.2rem; opacity: 0.9; font-weight: 300; margin-top: 10px; margin-bottom: 0;">Gestão de Educação<br>Especial Inclusiva</p>

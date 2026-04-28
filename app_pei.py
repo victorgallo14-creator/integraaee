c4e5060712f10795ee6cbe4e3582f6c37d3ad25c
victorgallo14-creator <victorgallo14@gmail.com>
2026-04-28 11:47:19 -0300

    Update app_pei.py

--- app_pei.py
+++ app_pei.py
@@ -6517,7 +6517,7 @@ elif modulo_atuacao == "🏫 Ensino Regular":
                                 pdf.cell(col_w[i], 6, h, 1, 0, 'C')
                             pdf.ln()
                             
-                            pdf.set_font("Arial", "", 5)
+                            pdf.set_font("Arial", "", 4)
                             lista_abaixo = data_ata.get('abaixo_basico', [])
                                 
                             def truncate_str(texto, max_w):
